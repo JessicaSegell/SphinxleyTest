083084ef72a3cc0134e3d8b6a551999f77fb6ca6
JessicaSegell <44054186+JessicaSegell@users.noreply.github.com>
2019-03-03 11:56:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+function welcome() {
+    let name = localStorage.getItem("nameinput");
+    if (name != null) {
+
+        namepls.style.display = "none";
+        welcomeback.style.display = "block";
+        document.getElementById("user").innerHTML = `${name}`;
+    }
+    else {
+        namepls.style.display = "block";
+    }
+}
+
+
+document.getElementById("btn").onclick = setName;
+
+function setName() {
+    let name = document.getElementById("nameinput").value;
+    localStorage.setItem("nameinput", name);
+    getName();
+}
+
+
+function getName() {
+    let name = localStorage.getItem("nameinput");
+    hiname.style.display = "block";
+    namepls.style.display = "none";
+    document.getElementById("username").innerHTML = ` ${name}!`;
+}
+
+//localStorage.removeItem("nameinput");
+
+
